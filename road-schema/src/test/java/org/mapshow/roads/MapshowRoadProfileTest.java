cffd41bd9cff4d7e6554bb57f91fe2e77b440c47
Helen Kwok <107752876+helenkwok@users.noreply.github.com>
2026-08-13 13:10:29 +0930

    Test restriction relation preprocessing

--- road-schema/src/test/java/org/mapshow/roads/MapshowRoadProfileTest.java
+++ road-schema/src/test/java/org/mapshow/roads/MapshowRoadProfileTest.java
@@ -1,10 +1,13 @@
 package org.mapshow.roads;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.carrotsearch.hppc.LongArrayList;
 import com.onthegomap.planetiler.reader.osm.OsmElement;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +26,25 @@ class MapshowRoadProfileTest {
     assertTrue(profile.splitOsmWayAtIntersections(way(2L, Map.of("highway", "primary"))));
     assertFalse(profile.splitOsmWayAtIntersections(way(3L, Map.of("highway", "footway"))));
     assertFalse(profile.splitOsmWayAtIntersections(way(4L, Map.of("highway", "residential", "area", "yes"))));
+  }
+
+  @Test
+  void preprocessesTurnRestrictionRelations() {
+    MapshowRoadProfile profile = new MapshowRoadProfile();
+    var relation = new OsmElement.Relation(
+      50L,
+      Map.of("type", "restriction", "restriction", "only_right_turn"),
+      List.of(
+        new OsmElement.Relation.Member(OsmElement.Type.WAY, 10L, "from"),
+        new OsmElement.Relation.Member(OsmElement.Type.NODE, 20L, "via"),
+        new OsmElement.Relation.Member(OsmElement.Type.WAY, 30L, "to")
+      )
+    );
+
+    var infos = profile.preprocessOsmRelation(relation);
+    assertEquals(1, infos.size());
+    var info = assertInstanceOf(TurnRestrictionInfo.class, infos.get(0));
+    assertEquals("only_right_turn", info.restriction());
+    assertEquals(20L, info.viaNode());
   }
 }
